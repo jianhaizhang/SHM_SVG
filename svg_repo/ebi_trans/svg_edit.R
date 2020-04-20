a98f2392854f5e19eee1346a24dee167f1c000c1
jianhaizhang <jzhan067@ucr.edu>
2020-04-20 12:58:40 -0700

    Add ontology attribute to tissues in each SVG image.

--- svg_repo/ebi_trans/svg_edit.R
+++ svg_repo/ebi_trans/svg_edit.R
@@ -1,7 +1,14 @@
 svg.edit <- function(path.in, path.out) {
 
-  library(XML)
+  library(XML); library(xml2)
   xmlfile <- xmlParse(path.in); xmltop <- xmlRoot(xmlfile)
+  spa <- xmlGetAttr(xmltop, 'xml:space')
+  # xml:space="preserve" results in emplty text nodes in the saved SVG image, so should be changed.
+  if (!is.null(spa)) if (spa=='preserve') { 
+
+    addAttributes(xmltop, 'xml:space'='none')
+  
+  }
   style <- 'fill:#62fafa;fill-opacity:1;stroke:#000000;stroke-width:0.21602426;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1'
   out <- xmltop[[xmlSize(xmltop)-1]]; ply <- xmltop[[xmlSize(xmltop)]]
   # Move base polygons to the top of the last group.
@@ -12,14 +19,17 @@ svg.edit <- function(path.in, path.out) {
   }; removeNodes(out) 
 
   # Change 'id' and 'style' of target polygons.
-  for (i in seq_len(xmlSize(ply))) {
- 
+  ids <- NULL; for (i in seq_len(xmlSize(ply))) {
+
     if (is.null(ply[[i]][[1]])) addAttributes(ply[[i]], style=style)
 
     # Apply to original svg code downloaded from 'https://github.com/ebi-gene-expression-group/anatomogram/tree/master/src/svg'. Assign names to each path or group.
     if (any(names(ply[[i]])=='title')) { 
 	  
-      id <- xmlValue(ply[[i]][['title']]); addAttributes(ply[[i]], id=id) 
+      id <- make.names(xmlValue(ply[[i]][['title']]))
+      ont <- xmlGetAttr(ply[[i]], 'id') 
+      addAttributes(ply[[i]], id=id); addAttributes(ply[[i]], ontology=ont)
+      names(id) <- ont; ids <- c(ids, id)
       removeNodes(ply[[i]][['title']]); addAttributes(ply[[i]], style=style)
   
     }
@@ -37,7 +47,7 @@ svg.edit <- function(path.in, path.out) {
       # Keep attributes of 'use' node unchanged to avoid polygon shifting in position.
       nod <- ply[[i]]; att <- xmlAttrs(nod); na <- names(att); val <- as.character(att); names(val) <- na
       w1 <- which(label %in% xmlGetAttr(nod, 'xlink:href')); id <- xmlGetAttr(nod, 'id')
-      ply[[i]] <- xmlClone(ply[[w1]], recursive=TRUE); addAttributes(ply[[i]], .attrs=val)
+      ply[[i]] <- xmlClone(ply[[w1]], recursive=TRUE); addAttributes(ply[[i]], .attrs=val); addAttributes(ply[[i]], ontology=id)
 
     }
 
@@ -54,18 +64,12 @@ svg.edit <- function(path.in, path.out) {
     if (length(w1)!=0) { cat(xmlGetAttr(nod, 'id'), ': \'use\' inside \'group\'', '\n') }
 
   }
-  
-  id <- NULL; for (i in seq_len(xmlSize(ply))) { 
-    
-    id0 <- make.names(xmlGetAttr(ply[[i]], 'id'))
-    xmlAttrs(ply[[i]])[['id']] <- id0
-    id <- c(id, id0) 
-
-  }; saveXML(xmlfile, file=pa); cat(pa, '\n'); return(id)
+  saveXML(xmlfile, file=pa); cat(pa, '\n'); return(ids)
 
 }
 
 svg <- list.files(path='/home/jzhan067/SVG_tutorial_file/anatomogram/src/svg/', pattern='.svg$', full.names=T)
+# In the svg tag, xml:space="preserve" introduces empty text nodes, e.g. mus_musculus.brain.svg. Thus this attribut should be removed.  
 # Avoid running this function to all SVG images, since some images have dislocated polygons and need adjustment.
 # for (i in svg[8]) svg.edit(path.in=i, path.out='/home/jzhan067/SVG_tutorial_file/anatomogram/src/svg/svg_trans')
 
@@ -101,10 +105,80 @@ for (path.in in svg1) {
 }
 
 
+# In original EBI SVG, some tissues have label/ontology id while others donot, so should not rely on label to add the ontology attribute. 
+path.out <- '~/test/'; for (i in svg) svg.edit(path.in=i, path.out=path.out)
 
+id.ont <- NULL; for (path.in in svg1) {
 
+  na <- strsplit(path.in, '/')[[1]]; na <- na[grep('.svg$', na)]
+  xmlfile <- xmlParse(path.in); xmltop <- xmlRoot(xmlfile)
+  ply <- xmltop[[xmlSize(xmltop)]]
+  path.in1 <- paste0(path.out, na) 
+  xmlfile1 <- xmlParse(path.in1); xmltop1 <- xmlRoot(xmlfile1)
+  ply1 <- xmltop1[[xmlSize(xmltop1)]]
 
+  ids <- NULL; for (j in seq_len(xmlSize(ply))) {
 
+    id <- xmlGetAttr(ply[[j]], 'id')
+    # lab <- xmlGetAttr(ply[[j]], 'label')
+    for (k in seq_len(xmlSize(ply1))) {
 
+      id1 <- xmlGetAttr(ply1[[k]], 'id')
+      # lab1 <- xmlGetAttr(ply1[[k]], 'label') 
+      # if (!is.null(lab) & !is.null(lab1) & is.null(xmlGetAttr(ply[[j]], 'href'))) { if (lab==lab1) { 
+        
+       # ont <- xmlGetAttr(ply1[[k]], 'ontology')
+       # addAttributes(ply[[j]], ontology=ont)
+       # names(id) <- ont; ids <- c(ids, id); break
 
+      # } } else { # If there are two "if" in front, they should be separated by "{}". Otherwise the "else" follows the 2nd "if".
 
+        if (id==id1) {
+
+          ont <- xmlGetAttr(ply1[[k]], 'ontology')
+          addAttributes(ply[[j]], ontology=ont)
+          names(id) <- ont; ids <- c(ids, id); break
+
+        }
+
+     # }
+
+    }
+
+  }; lis <- list(ids); names(lis) <- na; id.ont <- c(id.ont, lis)  
+  saveXML(xmlfile, file=path.in); cat(path.in, '\n')
+
+}
+
+library(rols)
+
+df <- NULL; for (i in seq_along(id.ont)) {
+
+  if (is.null(id.ont[[i]])) next
+  tissue <- id.ont[[i]]; tissue <- tissue[names(tissue)!='']
+  df0 <- as.data.frame(tissue, stringsAsFactors=FALSE)
+  df0$ontology <- rownames(df0); df0$SVG <- names(id.ont[i]) 
+  df <- rbind(df, df0)
+
+}; rownames(df) <- NULL
+
+df$description <- NA
+for (i in seq_len(nrow(df))) {
+
+  ont <- df[i, 'ontology']; abbr <- tolower(sub('_.*', '', ont))
+  trm <- tryCatch({ term(abbr, ont) }, error=function(e) { return(NA) })
+  if (is(trm, 'Term')) { des <- termDesc(trm); if (!is.null(des)) df[i, 'description'] <- termDesc(trm) }
+  if (!is(trm, 'Term')) next
+  
+}
+
+species='homo sapiens'; tissue='parotid.gland'
+
+retrunSVGFeature <- function(tissue, species) {
+
+sp <- sub(' |_|\\.|-', '|', species); tis <- sub(' |_|\\.|-', '|', tissue)
+w <- grepl(sp, df$SVG, ignore.case=TRUE) & grepl(tis, df$tissue, ignore.case=TRUE); return(df[w, ])
+
+}
+
+retrunSVGFeature(tissue='parotid.gland', species='homo sapiens')
